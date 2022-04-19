5bb25c522134d0b9c3d0aecd9c81a97a25bac40e
Faiz Saiyad <fsaiyad990@gmail.com>
2022-04-19 16:30:33 +0530

    Changed to PostgreSQL

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///sql-app.db"
+SQLALCHEMY_DATABASE_URI = "postgres://qgeuntlkoscyek:0faccf0da66f50acbe743f51deb10146cac62fcd54163e9a0fbb274c23afdf92@ec2-34-246-227-219.eu-west-1.compute.amazonaws.com:5432/d3gpbk693kb642"
 
 engine = create_engine(
         SQLALCHEMY_DATABASE_URI,
